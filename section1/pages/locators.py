0b31572544df37fd0fd6818da385e6090b91276f
Igor <riskyfel@gmail.com>
2019-09-14 22:07:04 +0300

    Урок 4.2 - 2 задание

---
+++ section1/pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    EMAIL_INPUT_FOR_LOGIN = (By.CSS_SELECTOR, "#id_login-username")
+    PASSWORD_INPUT_FOR_LOGIN = (By.CSS_SELECTOR, "id_login-password")
+    LINK_FORGOT_PASSWORD = (By.CSS_SELECTOR, "p>a")
+    LOGIN_BUTTON = (By.TAG_NAME, "[name='login_submit'']")
+    EMAIL_INPUT_FOR_REG = (By.CSS_SELECTOR, "id_registration-email")
+    PASSWORD_INPUT_FOR_REG = (By.CSS_SELECTOR, "id_registration-password1")
+    PASSWORD_INPUT_FOR_CONFIRM_REG = (By.CSS_SELECTOR, "id_registration-password2")
+    REG_BUTTON = (By.TAG_NAME, "[name='registration_submit']")
---
+++ section1/pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert self.browser.driver.current_url, "Login link not found"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.EMAIL_INPUT_FOR_LOGIN) and \
+               self.is_element_present(*LoginPageLocators.PASSWORD_INPUT_FOR_LOGIN) and\
+               self.is_element_present(*LoginPageLocators.LINK_FORGOT_PASSWORD) and\
+               self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Items not found"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.EMAIL_INPUT_FOR_REG) and\
+               self.is_element_present(*LoginPageLocators.PASSWORD_INPUT_FOR_REG) and\
+               self.is_element_present(*LoginPageLocators.PASSWORD_INPUT_FOR_CONFIRM_REG) and\
+               self.is_element_present(*LoginPageLocators.REG_BUTTON), "Items not found"
